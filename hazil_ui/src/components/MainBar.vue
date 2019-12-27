cc0d794456a925d6b96f7bb55b8da1cec20244ab
omershacham <Omer@cnvrg.io>
2019-12-27 12:55:53 +0200

    progress

--- hazil_ui/src/components/MainBar.vue
+++ hazil_ui/src/components/MainBar.vue
@@ -2,7 +2,10 @@
     <div class="bar">
         <div class="title">Hello {{ username }}</div>
         <div class="spacer"></div>
-        <div class="badge" @click="goToCart()"><font-awesome-icon icon="shopping-cart" /></div>
+        <div class="badge" @click="goToCart()">
+            <font-awesome-icon icon="shopping-cart"/>
+            <div class="small-badge" v-if="itemOnCart > 0">{{itemOnCart}}</div>
+        </div>
         <div class="badge"><img :src="profilePic"></div>
     </div>
 </template>
@@ -13,6 +16,10 @@
     export default {
         computed: {
             ...mapState('user', {username: 'username', profilePic: "profilePic"}),
+            ...mapState('products', ['products']),
+            itemOnCart() {
+                return Object.values(this.products).length
+            }
         },
         methods: {
             goToCart() {
@@ -32,20 +39,39 @@
         display: flex;
         align-items: baseline;
         border-bottom: 1px solid black;
+
         .title {
             flex: 0 1 auto;
         }
+
         .spacer {
             flex: auto;
         }
+
         .badge {
             flex: 0 0 0;
             margin-right: 1em;
+            position: relative;
+
+            .small-badge {
+                position: absolute;
+                border-radius: 5em;
+                height: 12px;
+                width: 12px;
+                background: blue;
+                color: white;
+                font-weight: 600;
+                top: -6px;
+                right: -6px;
+                font-size: 8px;
+            }
+
             img {
                 border-radius: 3em;
                 height: 32px;
                 width: 32px;
             }
+
             svg {
                 height: 24px;
                 width: 24px;
